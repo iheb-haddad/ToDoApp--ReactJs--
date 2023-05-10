efc08ed341901d491b6a8348c4a8424cb4f0a518
Iheb Hadded <hadediheb9@gmail.com>
2023-05-10 01:08:54 +0100

    Initial commit

---
+++ src/components/body/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Body.css'
+import { List} from "../index";
+
+const Body = () => {
+  return (
+    <div className='sub-container'>
+        <List />
+    </div>
+  )
+}
+
+export default Body
